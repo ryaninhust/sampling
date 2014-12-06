52310ebcb950242d4e5e49600b86761745e033b5
ryaninhust <yuanbwhust@gmail.com>
2014-12-06 14:50:48 +0800

    [Feature] Add algorithm example and rerrange egraphs

--- core.py
+++ core.py
@@ -1,26 +1,26 @@
 from validate import validate_sampling
-
 class AlgorithmNoResultError(Exception):
     pass
 
 
 class Algorithm(object):
-    seed_graph = None
-    validate_graph = None
+    egraph = None
 
-    def __init__(self, validate_graph):
-        self.validate_graph = validate_graph
+    def __init__(self, egraph):
+        self.egraph = egraph
+        self.sampled_graph = egraph.seed_graph.copy()
+        self.origin_graph = egraph.origin_graph
 
-    def run(self):
+    def run(self, k):
         raise NotImplementedError
 
     def _validate(self):
-        close_validate, kl = validate_sampling(self.graph, self.validate_graph)
+        close_validate, kl = validate_sampling(self.sampled_graph, self.origin_graph)
         return close_validate, kl
 
     def validate(self):
-        self.seed_graph = self.run()
-        if not self.seed_graph:
+        self.sampled_graph = self.run()
+        if not self.sampled_graph:
             raise AlgorithmNoResultError
         close_validate, kl = self._validate
         return close_validate, kl
